f17926c71efe3c4825b132263a538286624fe2e8
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-15 15:30:28 -0400

    finished styling

--- src/Main.js
+++ src/Main.js
@@ -4,73 +4,75 @@ import React, {Component} from 'react';
 function Main(){
 
     return(
-        <div className ='row'>
-            <div className='.col-md-5'>
-                <h1>project title or whatever</h1>
-                <table className='table'>
-                    <tr>
-                        <input type='text'/>
-                        <button>
-                            C
-                        </button>
-                    </tr>
-                    <tr>
-                        <button>
-                            1
-                        </button>
-                        <button>
-                            2
-                        </button>
-                        <button>
-                            3
-                        </button>
-                        <button>
-                            /
-                        </button>
-                    </tr>
-                    <tr>
-                        <button>
-                            4
-                        </button>
-                        <button>
-                            5
-                        </button>
-                        <button>
-                            6
-                        </button>
-                        <button>
-                            -
-                        </button>
-                    </tr>
-                    <tr>
-                        <button>
-                            7
-                        </button>
-                        <button>
-                            8
-                        </button>
-                        <button>
-                            9
-                        </button>
-                        <button>
-                            +
-                        </button>
-                    </tr>
-                    <tr>
-                        <button>
-                            .
-                        </button>
-                        <button>
-                            0
-                        </button>
-                        <button>
-                            =
-                        </button>
-                        <button>
-                            *
-                        </button>
-                    </tr>
-                </table>
+        <div className='container'>
+            <div className ='row'>
+                <div className='.col-md-5'>
+                    <h1>A Calculator!</h1>
+                    <table className='table'>
+                        <tr>
+                        <td colSpan={3}><input type='text'/></td>
+                        <td><button>
+                                C
+                            </button></td>
+                        </tr>
+                        <tr>
+                        <td><button>
+                                1
+                            </button></td>
+                            <td><button>
+                                2
+                            </button></td>
+                            <td><button>
+                                3
+                            </button></td>
+                            <td><button>
+                                /
+                            </button></td>
+                        </tr>
+                        <tr>
+                            <td><button>
+                                4
+                            </button></td>
+                            <td><button>
+                                5
+                            </button></td>
+                            <td><button>
+                                6
+                            </button></td>
+                            <td><button>
+                                -
+                            </button></td>
+                        </tr>
+                        <tr>
+                            <td><button>
+                                7
+                            </button></td>
+                            <td><button>
+                                8
+                            </button></td>
+                            <td><button>
+                                9
+                            </button></td>
+                            <td><button>
+                                +
+                            </button></td>
+                        </tr>
+                        <tr>
+                            <td><button>
+                                .
+                            </button></td>
+                            <td><button>
+                                0
+                            </button></td>
+                            <td><button>
+                                =
+                            </button></td>
+                            <td><button>
+                                *
+                            </button></td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     )
