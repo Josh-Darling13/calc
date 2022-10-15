e827b80f3ce199dcd6f6296a8d782bf0714e794f
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-15 17:50:33 -0400

    added event handlers and hooks

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 
 
 function Main(){
+
+    const [inputvalue, setinputvalue] = useState('');
+
+    function display(value){
+        /**
+         * takes value from buttons and passed it to 
+         * the text window
+         */
+        setinputvalue(inputvalue + value)
+
+    }
 
     return(
         <div className='container'>
@@ -10,64 +21,64 @@ function Main(){
                     <h1>A Calculator!</h1>
                     <table className='table'>
                         <tr>
-                        <td colSpan={3}><input type='text'/></td>
-                        <td><button>
+                        <td colSpan={3}><input type='text' value={inputvalue} /></td>
+                        <td><button onClick=''>
                                 C
                             </button></td>
                         </tr>
                         <tr>
-                        <td><button>
+                        <td><button onClick={()=>{display('1')}}>
                                 1
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('2')}}>
                                 2
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('3')}}>
                                 3
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('/')}}>
                                 /
                             </button></td>
                         </tr>
                         <tr>
-                            <td><button>
+                            <td><button onClick={()=>{display('4')}}>
                                 4
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('5')}}>
                                 5
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('6')}}>
                                 6
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('-')}}>
                                 -
                             </button></td>
                         </tr>
                         <tr>
-                            <td><button>
+                            <td><button onClick={()=>{display('7')}}>
                                 7
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('8')}}>
                                 8
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('9')}}>
                                 9
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('+')}}>
                                 +
                             </button></td>
                         </tr>
                         <tr>
-                            <td><button>
+                            <td><button onClick={()=>{display('')}}>
                                 .
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('0')}}>
                                 0
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('')}}>
                                 =
                             </button></td>
-                            <td><button>
+                            <td><button onClick={()=>{display('')}}>
                                 *
                             </button></td>
                         </tr>
